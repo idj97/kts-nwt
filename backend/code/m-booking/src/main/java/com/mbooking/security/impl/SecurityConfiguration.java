88633ca22c567f050914a1b888ed6e03276311fe
m-jordan <57231548+m-jordan@users.noreply.github.com>
2019-11-05 22:24:20 +0100

    security

---
+++ backend/code/m-booking/src/main/java/com/mbooking/security/impl/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.mbooking.security.impl;
+
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	
+}
