119d9fc378c701652634a2ae9bdacfd277a9a525
leonidas97 <ivkovicdjordje1997@gmail.com>
2019-12-13 20:11:33 +0100

    test fix

--- backend/code/m-booking/src/test/java/com/mbooking/service/LocationServiceUnitTest.java
+++ backend/code/m-booking/src/test/java/com/mbooking/service/LocationServiceUnitTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.mbooking.dto.LocationDTO;
@@ -26,6 +27,7 @@ import com.mbooking.repository.LocationRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test_h2")
 public class LocationServiceUnitTest {
 
 	@MockBean
--- backend/code/m-booking/src/test/java/com/mbooking/service/SectionServiceTest.java
+++ backend/code/m-booking/src/test/java/com/mbooking/service/SectionServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.mbooking.model.Section;
@@ -18,6 +19,7 @@ import com.mbooking.repository.SectionRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test_h2")
 public class SectionServiceTest {
 
 	@Autowired
