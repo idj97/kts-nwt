5d9804ef3e830433f4cc3732cebbf333545897eb
djordje <leonidas97>
2020-01-16 17:25:42 +0100

    added layout service integration tests #75

--- backend/code/m-booking/src/main/java/com/mbooking/service/impl/LayoutServiceImpl.java
+++ backend/code/m-booking/src/main/java/com/mbooking/service/impl/LayoutServiceImpl.java
@@ -7,12 +7,15 @@ import com.mbooking.repository.LayoutRepository;
 import com.mbooking.service.LayoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED)
 public class LayoutServiceImpl implements LayoutService {
 
     @Autowired
